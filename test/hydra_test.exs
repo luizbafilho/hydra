f71f4b320fd19fe9ef51062874b95ed1ee6f2111
DanX <danx.exe@gmail.com>
2016-05-12 15:44:05 -0300

    Nice error message for invalid URLs

--- test/hydra_test.exs
+++ test/hydra_test.exs
@@ -1,7 +1,33 @@
 defmodule HydraTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import ExUnit.CaptureIO
+
+  defmacro capture_shutdown(args) do
+    quote do
+      try do
+        unquote args[:do]
+      catch
+        :exit, :shutdown -> true
+      end
+    end
+  end
+
+  test "invalid URLs" do
+    urls = [
+      "//localhost:4000",
+      "http//localhost:4000",
+      "http://localhost:"
+    ]
+
+    for url <- urls do
+      output = capture_io(fn ->
+        capture_shutdown do
+          Hydra.CLI.main [url]
+        end
+      end)
+
+      assert output == "Invalid URL: #{url}\n"
+    end
   end
 end
